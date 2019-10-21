fbd06e65d41a45fe6afd3f286b45e7027ddfa225
Thomas Chin <tchin25@gmail.com>
2019-10-21 13:41:36 -0400

    made things prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="home">
-    <h1>This is home page</h1>
-    <v-img
-    src="../assets/mspi.png">
-    </v-img>
-    
+  <v-container class="home">
+    <v-row>
+      <v-col align="center">
+      <h1>This is home page</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="md4">
 
-  </div>
+      </v-col>
+      <v-col class="md4">
+        <v-img src="../assets/mspi.png"
+          max-height="200px"
+          contain
+        ></v-img>
+      </v-col>
+      <v-col class="md4">
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -1,40 +1,56 @@
 <template>
-  <div class="notifications">
-    <h1>This is notifications page</h1>
-    <v-btn v-on:click="listImages">Click Me</v-btn>
-    <v-img v-for="image in images" :key="image" :src="image.url" max-width="200"></v-img>
-  </div>
+  <v-container class="notification">
+    <v-row>
+      <v-col align="center">
+        <h1>This is notifications page</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align="center">
+        <v-btn v-on:click="listImages">Click Me</v-btn>
+      </v-col>
+    </v-row>
+    <v-row align-content="center" justify="center">
+      <v-img v-for="image in images" :key="image" :src="image.url" max-width="200"></v-img>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-const fb = require('../firebaseConfig.js')
- 
+const fb = require("../firebaseConfig.js");
+
 export default {
-  data: function()  {
+  data: function() {
     return {
-        images: []
-    }
+      images: []
+    };
   },
   mounted: function() {
-    this.$nextTick(function(){
+    this.$nextTick(function() {
       this.listImages();
-    })
+    });
   },
   methods: {
-    listImages: function(){
-      var childRef = fb.storageRef.child('images');
-      childRef.listAll().then((result) => {
-        result.items.forEach((imageRef) => {
-          imageRef.getDownloadURL().then((imageURL) => {
-            this.images.push( {url: imageURL.toString()} );
-          }).catch(function(error) {
-            // Handle any errors 
+    listImages: function() {
+      var childRef = fb.storageRef.child("images");
+      childRef
+        .listAll()
+        .then(result => {
+          result.items.forEach(imageRef => {
+            imageRef
+              .getDownloadURL()
+              .then(imageURL => {
+                this.images.push({ url: imageURL.toString() });
+              })
+              .catch(function(error) {
+                // Handle any errors
+              });
           });
-        });
-      }).catch(function(error) {
+        })
+        .catch(function(error) {
           // Handle any errors
-      });
+        });
     }
   }
-} 
+};
 </script> 
