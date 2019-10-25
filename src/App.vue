0ec0af959318488c25377cfe6f4cf2bd78fd1ae3
Thomas Chin <tchin25@gmail.com>
2019-10-25 16:12:15 -0400

    Updated Login. Need to install icons

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,8 @@ export default {
   }
 }
 </script>
+<style>
+html {
+    overflow: auto;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,56 @@
 <template>
-  <div id="login">
+  <!-- <div id="login">
     <h1>Login</h1>
     <input type="text" name="username" v-model="input.username" placeholder="Username" />
     <br>
     <input type="password" name="password" v-model="input.password" placeholder="Password" />
     <br>
     <button class="cyan darken-1" type="button" v-on:click="login()">Login</button>
+  </div>-->
+  <div class="container">
+    <v-row>
+      <v-col sm="4"></v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Login</v-card-title>
+          <v-card-text>
+            <div class="container">
+              <v-row>
+                <v-col sm="12">
+                  <v-text-field
+                    v-model="input.username"
+                    :rules="[rules.required]"
+                    label="Username"
+                    required
+                    dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col sm="12">
+                  <v-text-field
+                    v-model="input.password"
+                    :append-icon="show ? 'visibility' : 'visibility_off'"
+                    :rules="[rules.required]"
+                    :type="show ? 'text' : 'password'"
+                    label="Password"
+                    @click:append="show = !show"
+                    required
+                    dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col offset-sm="8">
+                  <v-btn small left color="primary" :click="login">Submit</v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col sm="4"></v-col>
+    </v-row>
   </div>
 </template>
 
@@ -14,9 +59,14 @@ export default {
   name: "Login",
   data() {
     return {
+      show: false,
       input: {
         username: "",
         password: ""
+      },
+      rules: {
+        required: value => !!value || "Required.",
+        emailMatch: () => "The email and password you entered don't match"
       }
     };
   },
