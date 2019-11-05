851e9f180ef2d8e3c493b6e82a01d8d644430dd4
Laura Smith <kersytelaura@gmail.com>
2019-11-04 23:24:46 -0500

    Added component to display video, edited imagecard component

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,13 +1,38 @@
 <template>
   <v-card tile max-width="200px">
-    <v-img :src="image" max-width="200px"></v-img>
-
+    <v-img :src="image" max-width="200px" @click.stop="dialog = true"
+></v-img>
     <v-card-subtitle>{{date}}</v-card-subtitle>
+    <v-dialog
+        v-model="dialog"
+        max-width="800"
+      >
+        <v-card>
+          <v-card-title class="headline">{{date}}</v-card-title>
+
+         <VideoCard />
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
+import VideoCard from "../components/VideoCard.vue"
 export default {
+  components: {
+    VideoCard
+  },
   props: {
     image: {
       type: String,
@@ -21,7 +46,15 @@ export default {
       type: String,
       default: "Title"
     }
-  }
+
+  },
+  data () {
+    return {
+      dialog: false,
+     
+    }
+  },
+
 };
 </script>
 
