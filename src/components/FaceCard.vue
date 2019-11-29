6c7f0ff3c071426bcc76696138bafe1b77ba7f56
Laura Smith <kersytelaura@gmail.com>
2019-11-29 15:09:25 -0500

    Webcam feed and capture is cleared after image is uploaded the user document is upadated with image name when image is uploaded/deleted

--- src/components/FaceCard.vue
+++ src/components/FaceCard.vue
@@ -12,6 +12,10 @@
 
 <script>
 const fb = require("../firebaseConfig.js");
+import { mapGetters } from "vuex";
+import store from "../store";
+import firebase from 'firebase';
+
 export default {
   
     name: "FaceCard",
@@ -29,19 +33,29 @@ export default {
             default: ""
         }
     },
-
+    computed: {
+    // map `this.user` to `this.$store.getters.user`
+    ...mapGetters({
+      user: "user"
+    })
+  },
     methods: {
         async deleteImage(event){
           // Create a reference to the file to delete
         var desertRef = fb.storageRef.child(this.imagePath);
+        var filesArray = fb.db.collection("users").doc(this.user.data.uid);
 
         // Delete the file
         await desertRef.delete().then(function() {
-            
+            console.log(this.image);
+           
             // File deleted successfully
         }).catch(function(error) {
             // Uh-oh, an error occurred!
         });  
+        await filesArray.update({
+                familiarFaces: firebase.firestore.FieldValue.arrayRemove(this.image)
+        });
         this.$emit('deletedImage');
         }
         
