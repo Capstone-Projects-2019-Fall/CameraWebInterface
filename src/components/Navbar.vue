4ff508583c60d56314f50b7452c56f809ea46d3e
Thomas Chin <tchin25@gmail.com>
2020-01-08 10:22:35 -0500

    Made homepage responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,83 +1,60 @@
 <template>
   <div id="navbar">
-
     <!-- Desktop navigation bar -->
     <v-toolbar class="cyan darken-1 hidden-xs-only">
-      <v-toolbar-title class="font-weight-black">  <v-img
-      src="../assets/mspi.png"
-      aspect-ratio="1"
-      max-height="30px"
-      contain
-      ></v-img>MSPi</v-toolbar-title>
+      <v-toolbar-title class="font-weight-medium"> MSPi</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items v-if="user.loggedIn"> <!-- if user is logged in !-->
-        <v-btn text
-          v-for="(item, i) in navLoggedIn"
-            :key="i"
-            :to = "item.route"
-            > {{item.text}} </v-btn>
-        
+      <v-toolbar-items v-if="user.loggedIn">
+        <!-- if user is logged in !-->
+        <v-btn text v-for="(item, i) in navLoggedIn" :key="i" :to="item.route">
+          {{ item.text }}
+        </v-btn>
+
         <v-btn text v-if="user.loggedIn" @click="signOut">Sign Out</v-btn>
       </v-toolbar-items>
 
-       <v-toolbar-items v-else> <!-- if user is not logged in !-->
-        <v-btn text
-          v-for="(item, i) in navLoggedOut"
-            :key="i"
-            :to = "item.route"
-            > {{item.text}} </v-btn>
-        </v-toolbar-items>
+      <v-toolbar-items v-else>
+        <!-- if user is not logged in !-->
+        <v-btn text v-for="(item, i) in navLoggedOut" :key="i" :to="item.route">
+          {{ item.text }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-    
 
     <!-- Mobile navigation bar -->
-     <v-toolbar class="hidden-sm-and-up cyan darken-1">
-        <v-toolbar-title class="font-weight-black">  <v-img
-      src="../assets/mspi.png"
-      aspect-ratio="1"
-      max-height="30px"
-      contain
-      ></v-img>MSPi</v-toolbar-title>
-        <v-spacer></v-spacer>
-          <v-menu bottom left>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                dark
-                icon
-                v-on="on"
-              >
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
-              <v-list v-if="user.loggedIn">
-              <v-list-item
-                v-for="(item, i) in navLoggedIn"
-                :key="i"
-                :to="item.route"
-              >
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="signOut">
-                <v-list-item-title>Sign Out</v-list-item-title>
-              </v-list-item>
-            </v-list>
+    <v-toolbar class="hidden-sm-and-up cyan darken-1">
+      <v-toolbar-title class="font-weight-medium"> MSPi</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn dark icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list v-if="user.loggedIn">
+          <v-list-item
+            v-for="(item, i) in navLoggedIn"
+            :key="i"
+            :to="item.route"
+          >
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="signOut">
+            <v-list-item-title>Sign Out</v-list-item-title>
+          </v-list-item>
+        </v-list>
 
-             <v-list v-else>
-              <v-list-item
-                v-for="(item, i) in navLoggedOut"
-                :key="i"
-                :to="item.route"
-              >
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item>
-         
- 
-            </v-list>
-            
-          </v-menu>
-
-      </v-toolbar>
-      
+        <v-list v-else>
+          <v-list-item
+            v-for="(item, i) in navLoggedOut"
+            :key="i"
+            :to="item.route"
+          >
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
   </div>
 </template>
 
@@ -86,68 +63,69 @@ import { mapGetters } from "vuex";
 const fb = require("../firebaseConfig.js");
 
 export default {
-  data () {
+  data() {
     return {
       dialog: false,
       navLoggedIn: [
         {
-          text: 'Home',
-          route: '/',
+          text: "Home",
+          route: "/",
           active: true
         },
         {
-          text: 'Live Video Stream',
-          route: '/livestream',
+          text: "Live Video Stream",
+          route: "/livestream",
           active: false
         },
         {
-          text: 'Notifications',
-          route: '/notifications',
-          active: false
-        },
-         {
-          text: 'Training',
-          route: '/training',
+          text: "Notifications",
+          route: "/notifications",
           active: false
         },
         {
-          text: 'Profile',
-          route: '/profile',
+          text: "Training",
+          route: "/training",
           active: false
         },
-
+        {
+          text: "Profile",
+          route: "/profile",
+          active: false
+        }
       ],
       navLoggedOut: [
-          {
-          text: 'Home',
-          route: '/',
+        {
+          text: "Home",
+          route: "/",
           active: true
         },
         {
-          text: 'Login',
-          route: '/login',
+          text: "Login",
+          route: "/login",
+          active: false
+        },
+        {
+          text: "Demo",
+          route: "/demo",
           active: false
         }
       ]
-    }
+    };
   },
   computed: {
     ...mapGetters({
-// map `this.user` to `this.$store.getters.user`
+      // map `this.user` to `this.$store.getters.user`
       user: "user"
     })
   },
   methods: {
     signOut() {
-      fb.auth
-        .signOut()
-        .then(() => {
-          this.$router.replace({
-            name: "home"
-          });
+      fb.auth.signOut().then(() => {
+        this.$router.replace({
+          name: "home"
         });
+      });
     }
   }
-}
+};
 </script>
-
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,108 +1,114 @@
 <template>
   <v-container class="home">
- 
-    <v-row class="pb-3">
-     
-      <v-card  >
-          <v-card-title class="cyan">What is MSPi?</v-card-title>
-          <v-row>
-            <v-col sm="2">
-              <v-img src="../assets/mspi.png"
-          max-height="50%"
-          contain
-          
-        ></v-img>
-            </v-col>
-            <v-col sm="10">
-          <v-card-text >
-            MSPi is a motion-capture security camera that provides users with live alerts as to what
-              occurs within and around their home. The target audience for this device is mainly suburban
-              and urban home owners as well as frequent Amazon shoppers. 
+    <v-row class="pb-3" justify="center">
+      <v-col sm="10" md="8">
+        <v-card class="elevation-0">
+          <v-card-title class="cyan pb-0"
+            ><v-spacer></v-spacer>
+            <p class="display-1">What is MSPi?</p>
+            <v-spacer></v-spacer
+          ></v-card-title>
+          <v-container fluid>
+            <v-row justify="center">
+              <v-col cols="auto" md="3">
+                <v-avatar class="ma-3" size="125" tile>
+                  <v-img src="../assets/mspi.png" contain></v-img>
+                </v-avatar>
+              </v-col>
+              <v-col cols="auto" md="9">
+                <v-card-text>
+                  <p class="headline">
+                    MSPi is a motion-capture security camera that provides users
+                    with live alerts as to what occurs within and around their
+                    home. The target audience for this device is mainly suburban
+                    and urban home owners as well as frequent Amazon shoppers.
+                  </p>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row class="pb-3 mt-8" justify="center">
+      <p class="display-1">
+        Features
+      </p>
+    </v-row>
+
+    <v-row class="pb-3" justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-cctv</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            Add mutiple cameras to your account to monitor multiple spots around
+            your home.
           </v-card-text>
-        </v-col>
-        </v-row>
-      </v-card>
-  </v-row>
+        </v-card>
+      </v-col>
 
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-face</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            Equipped with training module, the system allows you to train it to
+            recognize multiple faces. Know when your loved ones return home.
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-  <v-row class="pb-3">
-    
-    <v-card>
-      <v-card-title class="cyan">Features</v-card-title>
-    
-      <v-row>
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-motion-sensor</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            The system enabled with motion and human shape detection, so you
+            only get notified when a person approaches your home.
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-        <v-col sm="4">
-          <v-card flat>
-            <v-card-title class="justify-center"><v-icon large>mdi-cctv</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              Add mutiple cameras to your account to monitor multiple spots around your home.
-            </v-card-text>
-          </v-card>
-        </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-bell-ring</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            Get a notification via email when someone approches your home and
+            view all of you notifications via website or mobile application.
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-        <v-col sm="4">
-          <v-card flat>
-            <v-card-title class="justify-center"><v-icon large>mdi-face</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              Equipped with training module, the system allows you to train it to recognize multiple faces. 
-              Know when your loved ones return home.
-            </v-card-text>
-          </v-card>
-        </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-microphone</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            Communicate with the person at your doorstep using two-way
+            communication feature via website or mobile application.
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-        <v-col sm="4">
-          <v-card flat>
-            <v-card-title class="justify-center"><v-icon large>mdi-motion-sensor</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              The system enabled with motion and human shape detection, so you only get notified when a person
-              approaches your home.
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-      </v-row>
-
-      <v-row>
-
-        <v-col sm="4">
-          <v-card flat>
-            <v-card-title class="justify-center"><v-icon large>mdi-bell-ring</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              Get a notification via email when someone approches your home and view all of you notifications
-              via website or mobile application.
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col sm="4">
-          <v-card flat >
-            <v-card-title class="justify-center"><v-icon large>mdi-microphone</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              Communicate with the person at your doorstep using two-way communication feature via website or mobile application.
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col sm="4">
-          <v-card flat >
-            <v-card-title class="justify-center"><v-icon large>mdi-monitor-cellphone</v-icon></v-card-title>
-            <v-card-text class="black--text">
-              Monitor your home at any time using live video streaming. Connect via website or mobile application. 
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-
-    </v-card>
-  </v-row>
-
-   
-  
-
-    
-
-
+      <v-col cols="12" sm="6" md="4">
+        <v-card flat>
+          <v-card-title class="justify-center"
+            ><v-icon large>mdi-monitor-cellphone</v-icon></v-card-title
+          >
+          <v-card-text class="black--text">
+            Monitor your home at any time using live video streaming. Connect
+            via website or mobile application.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
-
