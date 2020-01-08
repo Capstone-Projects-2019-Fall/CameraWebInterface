6ccf0da199f5d9a508cbbf01610ea159bfacb99c
Thomas Chin <tchin25@gmail.com>
2020-01-08 10:49:19 -0500

    demo page completed

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,21 +1,48 @@
 <template>
-
+  <v-container>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col cols="auto">
+        <h1 class="display-2 py-4">MSPi Prototype Demo</h1>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row justify="center">
+      <iframe
+        src="https://docs.google.com/presentation/d/e/2PACX-1vR_Z5z3ZtQMjr1L6IkK9thZjDackmlZJ4h6c-UjhlZUzKRtpHLnG41HfFSXznlsE34hgjHaAW1OxDvA/embed?start=false&loop=true&delayms=5000"
+        frameborder="0"
+        width="960"
+        height="569"
+        allowfullscreen="true"
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+      ></iframe>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col cols="auto">
+        <h1 class="display-1 py-4">Livestream Demo</h1>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row justify="center">
+      <iframe
+        src="https://drive.google.com/file/d/1qULXrR8bSzfm6gHBsfbrolb_5EZErv2n/preview"
+        width="960"
+        height="542"
+      ></iframe>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
-
 export default {
-  components: { },
+  components: {},
   name: "Demo",
   data() {
     return {};
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
-<style>
-
-</style>
+<style></style>
